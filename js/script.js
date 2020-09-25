8624a343f4e89d6a188a89f889ddfe4ccf059c5b
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-09-25 17:06:05 +0300

    Add a function and some event handlers

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
-alert("Display message");
-prompt("Please enter your name", "Your name ..");
+function callIt() {
+    alert("Display message");
+    prompt("Please enter your name", "Your name ..");
+}
