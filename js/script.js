2fc1ca57f7124aa29c4b8e69c708ce2f80395628
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-09-25 19:25:30 +0300

    Add loadFunc function with onload event handler

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ function callIt(message) {
     alert("U clicked on: " + message);
     prompt("Please enter your name", "You clicked on: " + message);
 }
+
+function loadFunc() {
+    alert("The page has loaded. " + new Date());
+}
