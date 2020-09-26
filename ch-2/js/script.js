51eb3c48e19204e3f0979eaa7d04379415ed7d2d
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-09-26 15:45:48 +0300

    Ch-2
    Clean up code

--- ch-2/js/script.js
+++ ch-2/js/script.js
@@ -2,6 +2,32 @@ function sniffer(message) {
     console.log("Something happened: " + message);
 }
 
-document.getElementById("btn").onclick = function () {
+document.getElementById("main-html").onkeypress = function () {
+    sniffer("You pressed a key");
+};
+
+document.getElementById("main-body").onresize = function () {
+    sniffer("window resized");
+};
+
+document.getElementById("main-body").onload = function () {
+    alert("page loading");
+};
+
+document.getElementById("input-change").onchange = function () {
+    alert("It changed!");
+};
+
+document.getElementById("btn").onmouseover = function () {
     sniffer("Inner sniffer");
+};
+
+document.getElementById("second-paragraph").onmouseover = function () {
+    alert("Moused over second P");
+    console.log("Log the anonymous inner function on second paragraph");
+};
+
+document.getElementById("first-paragraph").onclick = function () {
+    alert("Clicked first P");
+    console.log("Log the anonymous inner function on first paragraph");
 };
