11341e795c884d17cc8ac944d08503c827cac864
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-09-27 09:41:52 +0300

    Ch-3
    Add onclick event on second paragraph

--- ch-3/js/script.js
+++ ch-3/js/script.js
@@ -1,7 +1,11 @@
 function sniffer(message) {
-    console.log("Something happened: " + message);
+    console.log("Event: " + message);
 }
 
 document.getElementById("btn").onmouseover = function () {
     sniffer("You moused over the button");
+};
+
+document.getElementById("paragraph-second").onclick = function () {
+    sniffer("Clicked on second paragraph");
 };
