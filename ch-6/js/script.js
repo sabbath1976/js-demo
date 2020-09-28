5c6e6dc9b8239e8b9199d2fc3983e5b5bb45832f
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-09-28 18:31:43 +0300

    Ch-6
    Add indexOf function with conditionals

--- ch-6/js/script.js
+++ ch-6/js/script.js
@@ -9,13 +9,29 @@ function runIt() {
     // console.log(document.getElementById("first").innerHTML);
     // var currentValue = document.getElementById("first").innerHTML;
     // console.log(currentValue);
-    var target = document.getElementById("first");
-    var currentValue = target.innerHTML;
-    console.log(currentValue);
+    //     var target = document.getElementById("first");
+    //     var currentValue = target.innerHTML;
+    //     console.log(currentValue);
 
-    if (1 < 4) {
-        // document.getElementById("first").innerHTML = "It is!";
-        target.innerHTML = "It isssssssssss!";
+    //     if (1 < 4) {
+    //         // document.getElementById("first").innerHTML = "It is!";
+    //         target.innerHTML = "It isssssssssss!";
+    //     }
+
+    // console.log("This is it".indexOf("@"));
+    var str = "This is it";
+    // console.log(str.indexOf("@"));
+    var result = str.indexOf("t");
+    console.log(result);
+
+    if (result != -1) {
+        console.log("Result is: " + result);
+        console.log(true);
+        alert("The occurence was found at position " + result);
+    } else {
+        console.log("Result is: " + result);
+        console.log(false);
+        alert("The occurence was not found");
     }
 }
 
