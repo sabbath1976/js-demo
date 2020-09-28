a4b06fea236981f85f4cfb46c35605423a91a314
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-09-28 09:22:12 +0300

    Ch-5
    Add js integrated functions

--- ch-5/js/script.js
+++ ch-5/js/script.js
@@ -1,15 +1,41 @@
-function sniffer(message) {
-    console.log("Event: " + message);
-    alert("inside function alert");
+// function sniffer(message) {
+//     console.log("Event: " + message);
+//     alert("inside function alert");
+// }
+
+// function outSide() {
+//     alert("Called: outSide() function");
+// }
+
+// // alert("not in function code.");
+
+// document.getElementById("btn").onclick = function () {
+//     sniffer("clicked on button");
+//     outSide();
+// };
+
+// var email = "sabbath1976@gmail.com";
+// var theAt = email.indexOf(".");
+
+// console.log(theAt);
+
+// alert("Alert: " + "sabbath1976@gmail.com".indexOf("."));
+
+function numberCheck() {
+    var myNumber = "cookies";
+    // var result = isNaN(myNumber);
+    console.log(isNaN(myNumber));
 }
 
-function outSide() {
-    alert("Called: outSide() function");
+function intCheck() {
+    var myNumber = "32 cookies";
+    var result = parseInt(myNumber);
+    console.log(result);
+    console.log(result + 32);
+    console.log(myNumber + 32);
 }
-
-alert("not in function code.");
 
 document.getElementById("btn").onclick = function () {
-    sniffer("clicked on button");
-    outSide();
+    numberCheck();
+    // intCheck();
 };
