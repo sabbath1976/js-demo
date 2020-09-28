a5ac88fc1fcec6542662bca280f5d69ba002e038
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-09-28 16:00:40 +0300

    Ch-6
    Add innerHTML property

--- ch-6/js/script.js
+++ ch-6/js/script.js
@@ -1,6 +1,19 @@
-if (1 > 4) {
-    console.log("It's true, so display this text!");
-    alert("It's true");
-} else {
-    console.log("It's not true");
+// if (1 > 4) {
+//     console.log("It's true, so display this text!");
+//     alert("It's true");
+// } else {
+//     console.log("It's not true");
+// }
+
+function runIt() {
+    // console.log(document.getElementById("first").innerHTML);
+    // var currentValue = document.getElementById("first").innerHTML;
+    // console.log(currentValue);
+    var target = document.getElementById("first");
+    var currentValue = target.innerHTML;
+    console.log(currentValue);
 }
+
+document.getElementById("btn").onclick = function () {
+    runIt();
+};
