2890c7ccb3fc13de9fe99d57ec87b88a25779b79
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-09-29 09:53:23 +0300

    Ch-7
    Add an if statement inside of a while loop

--- ch-7/js/script.js
+++ ch-7/js/script.js
@@ -1,16 +1,33 @@
+// function doItAgain() {
+//     var loopCount = 5;
+
+//     while (loopCount > 0) {
+//         console.log("LoopCount is now: " + loopCount);
+//         // loopCount = loopCount - 1;
+//         // loopCount -= 1;
+//         loopCount--;
+//     }
+
+//     console.log("End loop");
+// }
+
 function doItAgain() {
     var loopCount = 5;
 
     while (loopCount > 0) {
-        console.log("LoopCount is now: " + loopCount);
-        // loopCount = loopCount - 1;
-        // loopCount -= 1;
-        loopCount--;
+        if (loopCount == 3) {
+            alert("Three!");
+            loopCount--;
+        } else {
+            console.log("LoopCount is now: " + loopCount);
+            loopCount--;
+        }
     }
 
-    console.log("End loop");
+    console.log("End loop!");
 }
 
 document.getElementById("looper").onclick = function () {
     doItAgain();
+    this.style.color = "red";
 };
