be44adda94545508d4986742edcd0a3efb8086a4
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-09-29 17:01:47 +0300

    Ch-7
    For loop

--- ch-7/js/script.js
+++ ch-7/js/script.js
@@ -27,28 +27,44 @@
 //     console.log("End loop!");
 // }
 
+// function doItAgain() {
+//     var loopCount = 5;
+
+//     while (loopCount > 0) {
+//         var target = document.getElementById("target");
+
+//         // target.innerHTML =
+//         //     "LoopCount: " + loopCount + "<br>" + target.innerHTML;
+
+//         // target.innerHTML += loopCount + " LoopCount" + "<br>";
+//         // target.innerHTML = target.innerHTML + loopCount + "<br>";
+//         target.innerHTML = loopCount + "<br>" + target.innerHTML;
+//         console.log("LoopCount is now: " + loopCount);
+//         loopCount--;
+//     }
+
+//     console.log("End loop");
+
+//     var button = document.getElementById("looper");
+
+//     button.innerText = "Done looping!";
+//     button.style.fontSize = "34px";
+// }
+
 function doItAgain() {
-    var loopCount = 5;
-
-    while (loopCount > 0) {
+    for (var i = 10; i >= 0; i--) {
         var target = document.getElementById("target");
+        // target.innerHTML += i + " LoopCount" + "<br>";
+        target.innerHTML = "LoopCount: " + i + "<br>" + target.innerHTML;
 
-        // target.innerHTML =
-        //     "LoopCount: " + loopCount + "<br>" + target.innerHTML;
+        console.log("LoopCount is now: " + i);
 
-        // target.innerHTML += loopCount + " LoopCount" + "<br>";
-        // target.innerHTML = target.innerHTML + loopCount + "<br>";
-        target.innerHTML = loopCount + "<br>" + target.innerHTML;
-        console.log("LoopCount is now: " + loopCount);
-        loopCount--;
+        var button = document.getElementById("looper");
+        button.innerText = "Done looping!";
+        button.style.fontSize = "34px";
     }
 
-    console.log("End loop");
-
-    var button = document.getElementById("looper");
-
-    button.innerText = "Done looping!";
-    button.style.fontSize = "34px";
+    console.log("End for loop");
 }
 
 document.getElementById("looper").onclick = function () {
