a72ae3b3099a2e7e3759f31c9ac118f3f18ed126
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-09-29 15:45:30 +0300

    Ch-7
    Add innerHTML property to while loop

--- ch-7/js/script.js
+++ ch-7/js/script.js
@@ -11,20 +11,44 @@
 //     console.log("End loop");
 // }
 
+// function doItAgain() {
+//     var loopCount = 5;
+
+//     while (loopCount > 0) {
+//         if (loopCount == 3) {
+//             alert("Three!");
+//             loopCount--;
+//         } else {
+//             console.log("LoopCount is now: " + loopCount);
+//             loopCount--;
+//         }
+//     }
+
+//     console.log("End loop!");
+// }
+
 function doItAgain() {
     var loopCount = 5;
 
     while (loopCount > 0) {
-        if (loopCount == 3) {
-            alert("Three!");
-            loopCount--;
-        } else {
-            console.log("LoopCount is now: " + loopCount);
-            loopCount--;
-        }
+        var target = document.getElementById("target");
+
+        // target.innerHTML =
+        //     "LoopCount: " + loopCount + "<br>" + target.innerHTML;
+
+        // target.innerHTML += loopCount + " LoopCount" + "<br>";
+        // target.innerHTML = target.innerHTML + loopCount + "<br>";
+        target.innerHTML = loopCount + "<br>" + target.innerHTML;
+        console.log("LoopCount is now: " + loopCount);
+        loopCount--;
     }
 
-    console.log("End loop!");
+    console.log("End loop");
+
+    var button = document.getElementById("looper");
+
+    button.innerText = "Done looping!";
+    button.style.fontSize = "34px";
 }
 
 document.getElementById("looper").onclick = function () {
