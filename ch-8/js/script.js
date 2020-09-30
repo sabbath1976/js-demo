714b04f8991435d856f32521cf9588f3dcb241d1
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-09-30 14:46:13 +0300

    Ch-8
    Add length property

--- ch-8/js/script.js
+++ ch-8/js/script.js
@@ -1,6 +1,8 @@
 document.getElementById("btn").onclick = function () {
-    var dogs = ["German Shepherd ", 324432, " Beagle", " Boxer"];
+    var dogs = ["German Shepherd ", 324432, " Beagle", " Boxer", " Chimpdog"];
     var result = document.getElementById("target");
-    var pets = ["Pets", "Pets2", dogs, "Pets3"];
+    var pets = ["Pets", "Pets2", dogs, false];
     result.innerHTML = pets;
+
+    alert(dogs.length);
 };
