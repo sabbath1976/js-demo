4406f25240e61908e043772a94417d1814cfae69
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-09-30 18:44:37 +0300

    Add 2 arrays with methods and properties

--- ch-8/js/script.js
+++ ch-8/js/script.js
@@ -1,1 +1,20 @@
-alert(document.title);
+// alert(document.title);
+document.getElementById("btn").onclick = function () {
+    var dogs = ["German Shepherd", " Poodle", " Beagle", " Boxer"];
+    dogs.push(" Pushed Dog");
+    dogs.sort();
+
+    var pets = ["Pets", " Pets2 ", dogs, " Pets3"];
+    console.log("Pets length: " + pets.length);
+    console.log(pets);
+
+    var target = document.getElementById("target");
+    target.innerHTML = pets;
+
+    // alert(dogs.length);
+
+    for (var i = 0; i < pets.length; i++) {
+        // alert(dogs[i]);
+        console.log(pets[i]);
+    }
+};
