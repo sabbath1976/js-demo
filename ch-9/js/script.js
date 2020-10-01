d4e9529c02a96a5a869e1c904deb533377a371b6
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-10-01 15:38:27 +0300

    Ch-9
    Add a function that returns a value

--- ch-9/js/script.js
+++ ch-9/js/script.js
@@ -1,28 +1,8 @@
-function aFunction(aButton) {
-    var button = document.getElementById(aButton);
-    button.value = "It was clicked!!!";
-    // alert(rightButton.value);
+function singleReturn(argues) {
+    return "The function returned information: " + argues;
 }
 
-// function aFunction2() {
-//     var leftButton = document.getElementById("leftbutt");
-//     leftButton.value = "What?";
-//     // alert(rightButton.value);
-// }
-
-// function resetValue() {
-//     var rightButton = document.getElementById("rightbutt");
-//     rightButton.value = "Do it again!";
-// }
-
-document.getElementById("rightbutt").onclick = function () {
-    // alert(this.value);
-    aFunction(this.id);
-};
-
-document.getElementById("leftbutt").onclick = function () {
-    // alert(this.value);
-    // alert(document.title);
-    aFunction(this.id);
-    // resetValue();
+document.getElementById("btn").onclick = function () {
+    alert(singleReturn("Passing a message"));
+    console.log(singleReturn("Passing a message"));
 };
