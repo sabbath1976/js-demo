ee8d6f101259a30257badcd7113a37a92442cdcc
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-10-01 08:24:42 +0300

    Ch-9 More Functions

---
+++ ch-9/js/script.js
@@ -0,0 +1,7 @@
+var rightButton = document.getElementById("rightbutt");
+// rightButton.value = "what";
+// alert(rightButton.value);
+
+document.getElementById("leftbutt").onclick = function () {
+    alert(this.value);
+};
