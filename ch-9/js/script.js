50112b9600d0a8da96d42f6dc3450109c00cc1b5
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-10-01 16:21:35 +0300

    Ch-9
    Add 2 parameters to a returning function

--- ch-9/js/script.js
+++ ch-9/js/script.js
@@ -1,8 +1,13 @@
-function singleReturn(argues) {
-    return "The function returned information: " + argues;
+// function singleReturn(argues) {
+//     return "The function returned information: " + argues;
+// }
+
+function singleReturn(first, last) {
+    return "Your first name: " + first + "\nYour last name: " + last;
 }
 
 document.getElementById("btn").onclick = function () {
-    alert(singleReturn("Passing a message"));
-    console.log(singleReturn("Passing a message"));
+    // alert(singleReturn("Passing a message"));
+    // console.log(singleReturn("Passing a message"));
+    console.log(singleReturn("Thomas", "Kontogiannis"));
 };
