ee72d90fb2650bf8c52ec9ce298c7dbbffc916b8
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-10-01 08:42:54 +0300

    Ch-9
    Reuse function

--- ch-9/js/script.js
+++ ch-9/js/script.js
@@ -1,7 +1,22 @@
-var rightButton = document.getElementById("rightbutt");
-// rightButton.value = "what";
-// alert(rightButton.value);
+function aFunction() {
+    var rightButton = document.getElementById("rightbutt");
+    rightButton.value = "What?";
+    // alert(rightButton.value);
+}
+
+function resetValue() {
+    var rightButton = document.getElementById("rightbutt");
+    rightButton.value = "Do it again!";
+}
+
+document.getElementById("rightbutt").onclick = function () {
+    // alert(this.value);
+    aFunction();
+};
 
 document.getElementById("leftbutt").onclick = function () {
-    alert(this.value);
+    // alert(this.value);
+    // alert(document.title);
+    aFunction();
+    // resetValue();
 };
