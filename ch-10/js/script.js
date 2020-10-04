fa2fad0be111cdbb8ff144bd593b3dc361cbe1c3
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-10-04 14:39:38 +0300

    Ch-10
    Add 2nd way, innerHTML property

--- ch-10/js/script.js
+++ ch-10/js/script.js
@@ -1,9 +1,17 @@
-function myFunction() {
-    document.write("<h2>Hello World</h2>");
-}
+// function myFunction() {
+//     document.write("<h2>Hello World</h2>");
+// }
 
 // document.write("Hello World");
 
-document.getElementById("btn").onclick = function () {
-    myFunction();
-};
+var theText = "blad blah <h1>The blah</h1>";
+var targetDiv = document.getElementById("theDiv");
+// targetDiv.innerHTML = theText + targetDiv.innerHTML;
+// targetDiv.innerHTML = theText;
+targetDiv.innerHTML += theText;
+
+console.log(targetDiv.innerHTML);
+
+// document.getElementById("btn").onclick = function () {
+//     // myFunction();
+// };
