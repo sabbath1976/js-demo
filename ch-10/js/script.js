fba7fa8498fbd26bd1ecdb6ebd44dde33ce7994f
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-10-04 19:45:46 +0300

    Ch-10
    Add new content with JS into the page

--- ch-10/js/script.js
+++ ch-10/js/script.js
@@ -4,14 +4,34 @@
 
 // document.write("Hello World");
 
-var theText = "blad blah <h1>The blah</h1>";
-var targetDiv = document.getElementById("theDiv");
-// targetDiv.innerHTML = theText + targetDiv.innerHTML;
-// targetDiv.innerHTML = theText;
-targetDiv.innerHTML += theText;
+// var theText = "blad blah <h1>The blah</h1>";
+// var targetDiv = document.getElementById("theDiv");
+// // targetDiv.innerHTML = theText + targetDiv.innerHTML;
+// // targetDiv.innerHTML = theText;
+// targetDiv.innerHTML += theText;
 
-console.log(targetDiv.innerHTML);
+// console.log(targetDiv.innerHTML);
 
-// document.getElementById("btn").onclick = function () {
-//     // myFunction();
-// };
+function addNewContent() {
+    var para = document.createElement("p");
+    var node = document.createTextNode("The new text");
+    para.appendChild(node);
+
+    var targetDiv = document.getElementById("theDiv");
+    targetDiv.appendChild(para);
+
+    console.log(targetDiv.innerHTML);
+}
+
+function domWork() {
+    var targetDiv = document.getElementById("theDiv");
+    targetDiv.style.backgroundColor = "#bbbbf1";
+
+    console.log(targetDiv.innerHTML);
+}
+
+document.getElementById("btn").onclick = function () {
+    // myFunction();
+    // addNewContent();
+    domWork();
+};
