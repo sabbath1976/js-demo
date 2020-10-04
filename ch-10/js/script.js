3a71a664e8c4f2caa21486c3499c9e1a47b89e47
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-10-04 12:31:29 +0300

    Ch-10 HTML DOM
    Add document.write method

---
+++ ch-10/js/script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.write("<h2>Hello World</h2>");
+}
+
+// document.write("Hello World");
+
+document.getElementById("btn").onclick = function () {
+    myFunction();
+};
