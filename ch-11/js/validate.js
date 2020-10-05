1500820df104401f6ab6663cdbbfc3cb9e9a79ed
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-10-05 09:00:52 +0300

    Ch-11 HTML Forms

---
+++ ch-11/js/validate.js
@@ -0,0 +1,23 @@
+function validateForm() {
+    var nameFirst = document.getElementById("nameFirst");
+    var theValue = nameFirst.value;
+
+    if (theValue != "Stefan") {
+        console.log("Will this fire?");
+
+        var messageHolder = document.getElementById("target");
+        messageHolder.style.color = "red";
+        messageHolder.innerHTML = "<h2>Put in my name!</h2>";
+        nameFirst.select();
+
+        return false;
+    }
+}
+
+document.getElementById("submit").onclick = function () {
+    var doSubmit = validateForm();
+
+    if (doSubmit == false) {
+        return false;
+    }
+};
