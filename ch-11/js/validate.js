74c86d0dc9cb51a612846c7e9596ad21600c7373
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-10-05 13:38:56 +0300

    Ch-11
    Add validateForm() function

--- ch-11/js/validate.js
+++ ch-11/js/validate.js
@@ -29,3 +29,21 @@ document.getElementById("submit").onclick = function () {
         return false;
     }
 };
+
+function validateForm() {
+    var nameFirst = document.getElementById("nameFirst");
+    var theValue = nameFirst.value;
+
+    if (theValue != "Thomas") {
+        console.log("Will this fire?");
+
+        var messageHolder = document.getElementById("target");
+        messageHolder.style.color = "red";
+        messageHolder.innerHTML = "<h2>Put in my name!</h2>";
+
+        nameFirst.select();
+        // nameFirst.focus();
+
+        return false;
+    }
+}
