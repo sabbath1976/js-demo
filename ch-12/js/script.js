0e4060736ee31917d685a13bbfdb62bd0a3eefe1
Thomas Kontogiannis <sabbath1976@gmail.com>
2020-10-07 12:59:35 +0300

    Ch-12 Animation with JS
    Add getRandomColor() function

--- ch-12/js/script.js
+++ ch-12/js/script.js
@@ -19,27 +19,40 @@
 //     stopEdit();
 // }
 
-document.getElementById("whiteBoard").onclick = function () {
-    var canvas = document.getElementById("whiteBoard");
-    var context = canvas.getContext("2d");
+// document.getElementById("whiteBoard").onclick = function () {
+//     var canvas = document.getElementById("whiteBoard");
+//     var context = canvas.getContext("2d");
 
-    // set the line width and color (for all the lines)
-    context.lineWidth = 30;
-    context.strokeStyle = "rgb(205, 40, 40)";
+//     // set the line width and color (for all the lines)
+//     context.lineWidth = 30;
+//     context.strokeStyle = "rgb(205, 40, 40)";
 
-    // Draw the first line, with the standard butt ending
-    context.moveTo(25, 50); // (horizontal / vertical)
-    context.lineTo(400, 120);
-    context.lineCap = "butt";
-    context.stroke();
+//     // Draw the first line, with the standard butt ending
+//     context.moveTo(25, 50); // (horizontal / vertical)
+//     context.lineTo(400, 120);
+//     context.lineCap = "butt";
+//     context.stroke();
 
-    // Draw the second line. Change line color and line width
-    context.lineWidth = 25;
-    context.strokeStyle = "#95ca91";
+//     // Draw the second line. Change line color and line width
+//     context.lineWidth = 25;
+//     context.strokeStyle = "#95ca91";
 
-    context.beginPath();
-    context.moveTo(25, 120);
-    context.lineTo(400, 50);
-    context.lineCap = "butt";
-    context.stroke();
+//     context.beginPath();
+//     context.moveTo(25, 120);
+//     context.lineTo(400, 50);
+//     context.lineCap = "butt";
+//     context.stroke();
+// }
+
+var notRunning = true;
+
+function getRandomColor() {
+    var letters = "0123456789ABCDEF".split(" ");
+    var color = "#";
+
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
 }
